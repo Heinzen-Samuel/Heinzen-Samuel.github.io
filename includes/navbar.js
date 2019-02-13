4c59e9e7c19acd9c8394027f3a8a95236de3bf5c
Samuel Heinzen <38867551+Heinzen-Samuel@users.noreply.github.com>
2019-02-13 00:35:07 -0700

    MAJOR CHANGE: Site Overhaul
    Simply put I've change my direction with this website. Instead of having a homepage that links to seperate mini sites demonstratiing certain technologies, I'm going to attempt at building a blog site. This is inspired by a personal project to build a family server/blog spot.

---
+++ includes/navbar.js
@@ -0,0 +1,13 @@
+document.write('\
+\
+<ul>\
+	<li><a href="#">Homepage</a></li>\
+	<li><a href="#">Hawaii</a></li>\
+	<li><a href="#">Tennessee</a></li>\
+	<li><a href="#">Sandy</a></li>\
+	<li><a href="#">Fort Collins</a></li>\
+	<li><a href="#">Provo</a></li>\
+	<li><a href="#">Projects</a></li>\
+</ul>\
+\
+');
