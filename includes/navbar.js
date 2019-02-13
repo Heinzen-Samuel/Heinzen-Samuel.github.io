12d39a5152c198a023d863a05e901e0fc6d18ce3
Samuel Heinzen <38867551+Heinzen-Samuel@users.noreply.github.com>
2019-02-13 00:51:22 -0700

    made the navbar a single js file
    Done for simplicity

--- includes/navbar.js
+++ includes/navbar.js
@@ -1,13 +1,22 @@
 document.write('\
 \
-<ul>\
-	<li><a href="#">Homepage</a></li>\
-	<li><a href="#">Hawaii</a></li>\
-	<li><a href="#">Tennessee</a></li>\
-	<li><a href="#">Sandy</a></li>\
-	<li><a href="#">Fort Collins</a></li>\
-	<li><a href="#">Provo</a></li>\
-	<li><a href="#">Projects</a></li>\
-</ul>\
+	<div class="container">\
+		<div class="jumbotron">\
+		</div>\
+	</div>\
+\
+	<div class="container">\
+		<div class="nav">\
+			<ul>\
+				<li><a href="#">Homepage</a></li>\
+				<li><a href="#">Hawaii</a></li>\
+				<li><a href="#">Tennessee</a></li>\
+				<li><a href="#">Sandy</a></li>\
+				<li><a href="#">Fort Collins</a></li>\
+				<li><a href="#">Provo</a></li>\
+				<li><a href="#">Projects</a></li>\
+			</ul>\
+	    </div>\
+	</div>\
 \
 ');
