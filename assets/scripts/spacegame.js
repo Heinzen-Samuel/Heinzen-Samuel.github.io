e00f9761b7c96b3f894247878990f10744dc5cdb
Samuel Heinzen <38867551+Heinzen-Samuel@users.noreply.github.com>
2019-03-08 00:27:33 -0700

    Finished the Spacegame with proper graphics and function

--- assets/scripts/spacegame.js
+++ assets/scripts/spacegame.js
@@ -1,14 +1,11 @@
 var myGamePiece;
 var audio = document.getElementById("audio");
-var myObstacles = [];
-var astOne;
-var astTwo;
-var astThree;
+var myAsteroids = [];
 
 function startGame() {
     play();
     myGameArea.start();
-    myGamePiece = new component(30, 30, "red", 10, 120);  
+    myGamePiece = new ship(30, 30, "assets/img/spaceship.png", 280, 140, "image");  
 }
 
 function play(){
@@ -45,18 +42,30 @@ function everyinterval(n) {
   return false;
 }
 
-function component(width, height, color, x, y) {
+function ship(width, height, color, x, y, type) {
+  this.type = type;
+  if (type == "image") {
+    this.image = new Image();
+    this.image.src = color;
+  }
   this.width = width;
   this.height = height;
   this.speedX = 0;
   this.speedY = 0;
   this.x = x;
-  this.y = y; 
+  this.y = y;
 
   this.update = function(){
     ctx = myGameArea.context;
+    if (type == "image") {
+      ctx.drawImage(this.image, 
+        this.x, 
+        this.y,
+        this.width, this.height);
+    } else {
     ctx.fillStyle = color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
   }
 
   this.newPos = function() {
@@ -101,28 +110,95 @@ function component(width, height, color, x, y) {
     (myleft > otherright)) {
       crash = false;
     }
+
+    if (crash) {
+      myGamePiece.image.src = "assets/img/explosion.png";
+    }
+
     return crash;
   }
 }
 
+function asteroid(width, height, x, y, vertDir, horiDir, color, type) {
+  this.type = type;
+  if (type == "image") {
+    this.image = new Image();
+    this.image.src = color;
+  }
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.vertDir = vertDir;
+  this.horiDir = horiDir;
+
+  this.update = function(){
+    ctx = myGameArea.context;
+    if (type == "image") {
+      ctx.drawImage(this.image, 
+        this.x, 
+        this.y,
+        this.width, this.height);
+    } else {
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+}
+
 function updateGameArea() {
-  var x, y;
-  for (i = 0; i < myObstacles.length; i += 1) {
-    if (myGamePiece.crashWith(myObstacles[i])) {
+  var x, y, r, vd, hd;
+
+  for (i = 0; i < myAsteroids.length; i += 1) {
+    if (myGamePiece.crashWith(myAsteroids[i])) {
       myGameArea.stop();
       return;
     } 
   }
+
   myGameArea.clear();
   myGameArea.frameNo += 1;
-  if (myGameArea.frameNo == 1 || everyinterval(150)) {
-    x = myGameArea.canvas.width;
-    y = myGameArea.canvas.height - 200;
-    myObstacles.push(new component(10, 200, "green", x, y));
+  if (myGameArea.frameNo == 1 || everyinterval(30)) {
+    r = Math.floor(Math.random() * 4) + 1;
+
+    console.log(r);
+
+    switch(r) {
+      case 1:
+        y = 0;
+        x = getRndInteger(0, 570);
+        vd = 1.5;
+        hd = 0;
+        break;
+      case 2:
+        y = 300;
+        x = getRndInteger(0, 570);
+        vd = -1.5;
+        hd = 0;
+        break;
+      case 3:
+        x = 0;
+        y = getRndInteger(0, 270);
+        hd = 1.5;
+        vd = 0;
+        break;
+      case 4:
+        x = 600;
+        y = getRndInteger(0, 270);
+        hd = -1.5;
+        vd = 0;
+        break;
+      default:
+        console.log(r);
+    }
+
+    myAsteroids.push(new asteroid(30, 30, x, y, vd, hd, "assets/img/asteroid.png", "image"));
   }
-  for (i = 0; i < myObstacles.length; i += 1) {
-    myObstacles[i].x += -1;
-    myObstacles[i].update();
+
+  for (i = 0; i < myAsteroids.length; i += 1) {
+    myAsteroids[i].x += myAsteroids[i].horiDir;
+    myAsteroids[i].y += myAsteroids[i].vertDir;
+    myAsteroids[i].update();
   }
   
   myGamePiece.speedX = 0;
@@ -169,4 +245,8 @@ function moveright() {
 function clearmove() {
   myGamePiece.speedX = 0; 
   myGamePiece.speedY = 0; 
+}
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
